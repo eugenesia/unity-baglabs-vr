09109eaea53efd28308f2826eeeccb5095c2d59a
Eugene Sia <eugene@eugenesia.co.uk>
2016-11-21 14:45:10 +0000

    Upgrade to Unity 5.4.2f2-GVR12

--- Assets/Scripts/MobileInput.cs
+++ Assets/Scripts/MobileInput.cs
@@ -5,7 +5,7 @@ public class MobileInput : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		GvrViewer.Instance.Recenter();
 	}
 	
 	// Update is called once per frame
@@ -15,8 +15,20 @@ public class MobileInput : MonoBehaviour {
 			GameObject player = GameObject.FindGameObjectWithTag("Player");
 			GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
 
+			Debug.Log(camera.transform.forward);
+
 			// Change the character's position by moving in the direction that camera is facing.
-			player.transform.position = player.transform.position + camera.transform.forward;
+			Vector3 direction = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z);
+			player.transform.position = player.transform.position + direction;
 		}
+
+		// Update direction of debug pointer.
+		//GameObject camera2 = GameObject.FindGameObjectWithTag("MainCamera");
+		Camera camera2 = Camera.main;
+		GameObject debugPtrParent = GameObject.FindGameObjectWithTag("DebugPointerParent");
+
+		Vector3 direction2 = new Vector3(camera2.transform.forward.x, 0, camera2.transform.forward.z) * 1000;
+
+		debugPtrParent.transform.rotation = Quaternion.LookRotation(direction2);
 	}
 }
